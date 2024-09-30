784388f2097ff6b56964fc65c894d3c18692418a
JUNGSEUNGKIM <153578440+JUNGSEUNGKIM@users.noreply.github.com>
2024-09-30 14:14:46 +0900

    JS kakao test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,19 +7,20 @@ import Login from "./view/user/login";
 import LoginKakao from "./view/kakao/LoginKakao";
 import {Route, Routes} from "react-router-dom";
 import LoginHandler from "./JHkakao/Page/LoginHandler";
+import LoginComplete from "./JHkakao/Page/LoginComplete";
 
 function App() {
   const [message, setMessage] = useState('');
 
-  useEffect(() => {
-    axios.get(`${process.env.REACT_APP_API_URL}/hello`)
-        .then(response => {
-          setMessage(response.data);
-        })
-        .catch(error => {
-          console.error('There was an error fetching the data!', error);
-        });
-  }, []);
+  // useEffect(() => {
+  //   axios.get(`${process.env.REACT_APP_API_URL}/hello`)
+  //       .then(response => {
+  //         setMessage(response.data);
+  //       })
+  //       .catch(error => {
+  //         console.error('There was an error fetching the data!', error);
+  //       });
+  // }, []);
   return (
       <div className="App">
 
@@ -32,6 +33,7 @@ function App() {
                   path="/login/oauth2/callback/kakao" // 리디렉트 URL
                   element={<LoginHandler />} // 리디렉트 후 표시될 컴포넌트
               />
+              <Route path="/LoginComplete" element={<LoginComplete/>}></Route>
           </Routes>
       </div>
   );
--- frontend/src/JHkakao/Page/LoginHandler.js
+++ frontend/src/JHkakao/Page/LoginHandler.js
@@ -7,26 +7,28 @@ const LoginHandler = (props) => {
     const code = new URL(window.location.href).searchParams.get("code");
 
 //인가코드 백으로 보내는 코드
-//     useEffect(() => {
-//         const kakaoLogin = async () => {
-//             await axios({
-//                 method: "GET",
-//                 url: `${process.env.REACT_APP_REDIRECT_URL}/?code=${code}`,
-//                 headers: {
-//                     // "Content-Type": "application/json;charset=utf-8", //json형태로 데이터를 보내겠다는뜻인데 XML로 할거면 지워도되나?
-//                     "Access-Control-Allow-Origin": "*", //이건 cors 에러때문에 넣어둔것. 당신의 프로젝트에 맞게 지워도됨
-//                 },
-//             }).then((res) => { //백에서 완료후 우리사이트 전용 토큰 넘겨주는게 성공했다면
-//                 console.log(res);
-//                 //계속 쓸 정보들( ex: 이름) 등은 localStorage에 저장해두자
-//                 localStorage.setItem("name", res.data.account.kakaoName);
-//                 localStorage.setItem("nickname", res.data.account.nickName);
-//                 //로그인이 성공하면 이동할 페이지
-//                 navigate("/LoginComplete");
-//             });
-//         };
-//         kakaoLogin();
-//     }, [props.history]);
+    useEffect(() => {
+        const kakaoLogin = async () => {
+            await axios({
+                method: "GET",
+                url: `${process.env.REACT_APP_REDIRECT_URL}/?code=${code}`,
+                headers: {
+                    // "Content-Type": "application/json;charset=utf-8", //json형태로 데이터를 보내겠다는뜻인데 XML로 할거면 지워도되나?
+                    "Access-Control-Allow-Origin": "*", //이건 cors 에러때문에 넣어둔것. 당신의 프로젝트에 맞게 지워도됨
+                },
+            }).then((res) => { //백에서 완료후 우리사이트 전용 토큰 넘겨주는게 성공했다면
+                console.log(res);
+                //계속 쓸 정보들( ex: 이름) 등은 localStorage에 저장해두자
+                localStorage.setItem("name", res.data.account.kakaoName);
+                localStorage.setItem("nickname", res.data.account.nickName);
+                //로그인이 성공하면 이동할 페이지
+                navigate("/LoginComplete");
+            });
+        };
+        // kakaoLogin();
+        console.log(code)
+        navigate("/LoginComplete");
+    }, [props.history]);
 
     return (
         <div className="LoginHandeler">
